0223a4fee94ffe8b94fd8af457b429e7ce22fd60
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-03-20 17:17:15 +0900

    [level 0] Title: 로그인 성공?, Time: 0.37 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public string solution(string[] id_pw, string[,] db) {
+        string answer = "";
+        for(int i = 0; i<db.GetLength(0); i++){
+            if(id_pw[0] == db[i,0]){
+                if(id_pw[1]== db[i,1]){
+                    answer = "login";
+                }
+                else{
+                    answer = "wrong pw";
+                }
+            }
+        }
+        if (answer == ""){
+            answer = "fail";
+        }
+        return answer;
+    }
+}
