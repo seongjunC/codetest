8d94091fc910e857020fb06e3ae93a43d7dc97d6
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-03-20 16:39:39 +0900

    [Bronze V] Title: Hello World, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.cs
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -1,16 +1,1 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace codetest
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Hello World!");
-        }
-    }
-}
+Console.Write("Hello World!");
