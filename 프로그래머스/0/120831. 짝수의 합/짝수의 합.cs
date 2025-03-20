e14391a12b20317f40cdc6a5148224ce1126abef
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-03-20 16:05:51 +0900

    [level 0] Title: 짝수의 합, Time: 0.27 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i <= n; i++){
+            if(i % 2 == 0){
+                 sum += i;
+            }
+        }
+        answer = sum;
+        return answer;
+    }
+}
