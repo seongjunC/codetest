b956ac87db89204867f02dbb59bf8f9cb4a7b16f
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-03-20 16:10:26 +0900

    [level 0] Title: 세균 증식, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        for ( int i = 0; i < t; i++){
+            n *= 2;
+        }
+        answer = n;
+        return answer;
+    }
+}
